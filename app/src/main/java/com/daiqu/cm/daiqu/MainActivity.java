dcecf084938ee45e2fb3ed314444596ad2770c39
sunqi <1766649908@qq.com>
2017-07-23 17:09:35 +0800

    init

---
+++ app/src/main/java/com/daiqu/cm/daiqu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.daiqu.cm.daiqu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
