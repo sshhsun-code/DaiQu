80f37e06dfc92fb46b3759567bdacbde74bd0c11
zhipeng <123123123>
2017-07-26 14:50:10 +0800

    [发单界面] 增加价格选择按钮，快递公司数组

--- app/src/main/java/com/daiqu/cm/daiqu/fragment/SendFragment.java
+++ app/src/main/java/com/daiqu/cm/daiqu/fragment/SendFragment.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 import com.daiqu.cm.daiqu.R;
@@ -17,7 +19,7 @@ import com.daiqu.cm.daiqu.R;
  * Created by CM on 2017/7/25.
  */
 
-public class SendFragment extends Fragment {
+public class SendFragment extends Fragment implements View.OnClickListener{
 
     public static SendFragment newInstance() {
 
@@ -32,13 +34,17 @@ public class SendFragment extends Fragment {
     private EditText name_edit;
     private EditText phone_edit;
     private Spinner company_spinner;
+    private Button pick_uo_address_btn;
     private EditText pick_up_address;
     private EditText pick_up_code;
     private EditText arrive_address;
+    private Button arrive_address_btn;
     private EditText arrive_time_begin;
     private EditText arrive_time_end;
     private Button confirm_btn;
-
+    private RadioGroup radioGroup;
+    private RadioButton price_small_rb;
+    private RadioButton price_big_rb;
 
     @Nullable
     @Override
@@ -46,31 +52,64 @@ public class SendFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.send_fragment,container,false);
         backButton = view.findViewById(R.id.send_back);
-        backButton.setOnClickListener(back_clickListener);
+
 
         name_edit = view.findViewById(R.id.send_name_edit);
         phone_edit = view.findViewById(R.id.send_phone_editText);
         company_spinner = view.findViewById(R.id.send_company_spinner);
         pick_up_address = view.findViewById(R.id.send_pick_up_address_edit);
+        pick_uo_address_btn = view.findViewById(R.id.send_pick_up_btn);
         pick_up_code = view.findViewById(R.id.send_code_editText);
         arrive_address = view.findViewById(R.id.send_arrive_address_edit);
+        arrive_address_btn = view.findViewById(R.id.send_arrive_address_btn);
         arrive_time_begin = view.findViewById(R.id.send_arrive_time_edit_begin);
         arrive_time_end = view.findViewById(R.id.send_arrive_time_edit_end);
         confirm_btn = view.findViewById(R.id.send_confirm_btn);
+        radioGroup = view.findViewById(R.id.send_price_radioGroup);
+
+
+
+        backButton.setOnClickListener(this);
+        pick_up_address.setOnClickListener(this);
+        arrive_address.setOnClickListener(this);
+        arrive_time_begin.setOnClickListener(this);
+        arrive_time_end.setOnClickListener(this);
 
         return view;
     }
 
-    private View.OnClickListener back_clickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            FragmentTransaction beginTransaction = getFragmentManager().beginTransaction();
-            beginTransaction.replace(R.id.main_content,
-                    HomeFragment.newInstance(getString(R.string.home)));
-            beginTransaction.commit();
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.send_pick_up_address_edit:
+
+                break;
+            case R.id.send_pick_up_btn:
+
+                break;
+            case R.id.send_arrive_address_btn:
+
+                break;
+            case R.id.send_arrive_address_edit:
+
+                break;
+            case R.id.send_back:
+                FragmentTransaction beginTransaction = getFragmentManager().beginTransaction();
+                beginTransaction.replace(R.id.main_content,
+                        HomeFragment.newInstance(getString(R.string.home)));
+                beginTransaction.commit();
+                break;
+            case R.id.send_confirm_btn:
+
+                break;
+            case R.id.send_arrive_time_edit_begin:
+
+                break;
+            case R.id.send_arrive_time_edit_end:
+
+                break;
         }
-    };
-
-
-
+    }
 }
